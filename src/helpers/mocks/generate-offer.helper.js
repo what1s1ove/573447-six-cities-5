ff2a102335442c39a0dd0c545d1f6a62d56c2e19
what1s1ove <zubko.2007@gmail.com>
2020-10-09 08:20:49 +0300

    module3-task1: + generate city & title for offer-mock

--- src/helpers/mocks/generate-offer.helper.js
+++ src/helpers/mocks/generate-offer.helper.js
@@ -1,7 +1,20 @@
+import {getRandomItem} from '~/helpers/array/array';
 import {generateRandomId} from '~/helpers/mocks/generate-random-id.helper';
+import {OfferCity} from '~/common/enums/enums';
+
+const TITLES = [
+  `Beautiful \u0026 luxurious apartment at great location`,
+  `Wood and stone place`,
+  `Canal View Prinsengracht`,
+  `Nice, cozy, warm big bed apartment`,
+];
+
+const cities = Object.values(OfferCity);
 
 const generateOffer = () => ({
   id: generateRandomId(),
+  city: getRandomItem(cities),
+  title: getRandomItem(TITLES),
 });
 
 export {generateOffer};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app/app';
+import App from '~/components/app/app';
+import {generateOffers} from '~/helpers/helpers';
 
 const AppConfig = {
-  OFFERS_COUNT: 322,
+  OFFERS_COUNT: 4,
 };
+
+const offers = generateOffers(AppConfig.OFFERS_COUNT);
+
+console.log(offers);
 
 ReactDOM.render(
     <App offersCount={AppConfig.OFFERS_COUNT} />,
