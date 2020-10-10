5e2719e79d45d2a2778df66ad5aabb15aa66532d
what1s1ove <zubko.2007@gmail.com>
2020-10-10 18:58:15 +0300

    module3-task1: * offer-screen component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {AppRoute} from '~/common/enums/enums';
-import {offerType} from '~/common/prop-types/prop-types';
+import {offerType, reviewType} from '~/common/prop-types/prop-types';
 import MainScreen from '../main-screen/main-screen';
 import AuthScreen from '../auth-screen/auth-screen';
 import FavoritesScreen from '../favorites-screen/favorites-screen';
 import OfferScreen from '../offer-screen/offer-screen';
 
-const App = ({offers}) => (
+const App = ({offers, reviews}) => (
   <BrowserRouter>
     <Switch>
       <Route path={AppRoute.MAIN} exact>
@@ -21,7 +21,7 @@ const App = ({offers}) => (
         <FavoritesScreen offers={offers} />
       </Route>
       <Route path={AppRoute.OFFER_ID} exact>
-        <OfferScreen offers={offers} />
+        <OfferScreen offers={offers} reviews={reviews} />
       </Route>
     </Switch>
   </BrowserRouter>
@@ -29,6 +29,7 @@ const App = ({offers}) => (
 
 App.propTypes = {
   offers: PropTypes.arrayOf(offerType).isRequired,
+  reviews: PropTypes.arrayOf(reviewType).isRequired,
 };
 
 export default App;
--- src/components/offer-screen/offer-screen.jsx
+++ src/components/offer-screen/offer-screen.jsx
@@ -1,12 +1,33 @@
 import * as React from 'react';
+import {useParams} from 'react-router-dom';
 import PropTypes from 'prop-types';
-import {offerType} from '~/common/prop-types/prop-types';
+import clsx from 'clsx';
+import {offerType, reviewType} from '~/common/prop-types/prop-types';
+import {getRatingInPercents} from '~/helpers/helpers';
 import Header from '~/components/header/header';
 import OfferList from '~/components/offer-list/offer-list';
+import OfferGalleryList from '~/components/offer-gallery-list/offer-gallery-list';
+import OfferGoodsList from '~/components/offer-goods-list/offer-goods-list';
+import ReviewList from '~/components/review-list/review-list';
+import ReviewForm from '~/components/review-form/review-form';
+import {getOfferById} from './helpers';
 
 const MAX_SIMILAR_OFFERS_COUNT = 3;
 
-const OfferScreen = ({offers}) => {
+const OfferScreen = ({offers, reviews}) => {
+  const [offer, setOffer] = React.useState(null);
+  const params = useParams();
+
+  React.useEffect(() => {
+    const offerById = getOfferById(offers, params.id) || null;
+
+    setOffer(offerById);
+  }, [params.id]);
+
+  if (!offer) {
+    return null;
+  }
+
   const similarOffers = offers.slice(0, MAX_SIMILAR_OFFERS_COUNT);
 
   return (
@@ -14,61 +35,16 @@ const OfferScreen = ({offers}) => {
       <Header />
       <main className="page__main page__main--property">
         <section className="property">
-          <div className="property__gallery-container container">
-            <div className="property__gallery">
-              <div className="property__image-wrapper">
-                <img
-                  className="property__image"
-                  src="img/room.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="property__image-wrapper">
-                <img
-                  className="property__image"
-                  src="img/apartment-01.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="property__image-wrapper">
-                <img
-                  className="property__image"
-                  src="img/apartment-02.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="property__image-wrapper">
-                <img
-                  className="property__image"
-                  src="img/apartment-03.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="property__image-wrapper">
-                <img
-                  className="property__image"
-                  src="img/studio-01.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="property__image-wrapper">
-                <img
-                  className="property__image"
-                  src="img/apartment-01.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-            </div>
-          </div>
+          <OfferGalleryList imgPaths={offer.images} />
           <div className="property__container container">
             <div className="property__wrapper">
-              <div className="property__mark">
-                <span>Premium</span>
-              </div>
+              {offer.isPremium && (
+                <div className="property__mark">
+                  <span>Premium</span>
+                </div>
+              )}
               <div className="property__name-wrapper">
-                <h1 className="property__name">
-                  Beautiful &amp; luxurious studio at great location
-                </h1>
+                <h1 className="property__name">{offer.title}</h1>
                 <button
                   className="property__bookmark-button button"
                   type="button"
@@ -87,226 +63,65 @@ const OfferScreen = ({offers}) => {
                 <div className="property__stars rating__stars">
                   <span
                     style={{
-                      width: `80%`,
+                      width: `${getRatingInPercents(offer.rating)}%`,
                     }}
                   />
                   <span className="visually-hidden">Rating</span>
                 </div>
                 <span className="property__rating-value rating__value">
-                  4.8
+                  {offer.rating}
                 </span>
               </div>
               <ul className="property__features">
                 <li className="property__feature property__feature--entire">
-                  Apartment
+                  {offer.type}
                 </li>
                 <li className="property__feature property__feature--bedrooms">
-                  3 Bedrooms
+                  {offer.bedroomCount} Bedrooms
                 </li>
                 <li className="property__feature property__feature--adults">
-                  Max 4 adults
+                  Max {offer.maxAdultsCount} adults
                 </li>
               </ul>
               <div className="property__price">
-                <b className="property__price-value">&euro;120</b>
+                <b className="property__price-value">&euro;{offer.price}</b>
                 <span className="property__price-text">&nbsp;night</span>
               </div>
               <div className="property__inside">
                 <h2 className="property__inside-title">What&apos;s inside</h2>
-                <ul className="property__inside-list">
-                  <li className="property__inside-item">Wi-Fi</li>
-                  <li className="property__inside-item">Washing machine</li>
-                  <li className="property__inside-item">Towels</li>
-                  <li className="property__inside-item">Heating</li>
-                  <li className="property__inside-item">Coffee machine</li>
-                  <li className="property__inside-item">Baby seat</li>
-                  <li className="property__inside-item">Kitchen</li>
-                  <li className="property__inside-item">Dishwasher</li>
-                  <li className="property__inside-item">Cabel TV</li>
-                  <li className="property__inside-item">Fridge</li>
-                </ul>
+                <OfferGoodsList goods={offer.goods} />
               </div>
               <div className="property__host">
                 <h2 className="property__host-title">Meet the host</h2>
                 <div className="property__host-user user">
-                  <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
+                  <div
+                    className={clsx(
+                        `property__avatar-wrapper user__avatar-wrapper`,
+                        offer.host.isPro && `property__avatar-wrapper--pro`
+                    )}
+                  >
                     <img
                       className="property__avatar user__avatar"
-                      src="img/avatar-angelina.jpg"
+                      src={offer.host.avatarUrl}
                       width="74"
                       height="74"
                       alt="Host avatar"
                     />
                   </div>
-                  <span className="property__user-name"> Angelina </span>
+                  <span className="property__user-name">{offer.host.name}</span>
                 </div>
                 <div className="property__description">
                   <p className="property__text">
-                    A quiet cozy and picturesque that hides behind a a river by
-                    the unique lightness of Amsterdam. The building is green and
-                    from 18th century.
-                  </p>
-                  <p className="property__text">
-                    An independent House, strategically located between Rembrand
-                    Square and National Opera, but where the bustle of the city
-                    comes to rest in this alley flowery and colorful.
+                    {offer.description}
                   </p>
                 </div>
               </div>
               <section className="property__reviews reviews">
                 <h2 className="reviews__title">
-                  Reviews &middot; <span className="reviews__amount">1</span>
+                  Reviews &middot; <span className="reviews__amount">{reviews.length}</span>
                 </h2>
-                <ul className="reviews__list">
-                  <li className="reviews__item">
-                    <div className="reviews__user user">
-                      <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                        <img
-                          className="reviews__avatar user__avatar"
-                          src="img/avatar-max.jpg"
-                          width="54"
-                          height="54"
-                          alt="Reviews avatar"
-                        />
-                      </div>
-                      <span className="reviews__user-name"> Max </span>
-                    </div>
-                    <div className="reviews__info">
-                      <div className="reviews__rating rating">
-                        <div className="reviews__stars rating__stars">
-                          <span
-                            style={{
-                              width: `80%`,
-                            }}
-                          ></span>
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <p className="reviews__text">
-                        A quiet cozy and picturesque that hides behind a a river
-                        by the unique lightness of Amsterdam. The building is
-                        green and from 18th century.
-                      </p>
-                      <time className="reviews__time" dateTime="2019-04-24">
-                        April 2019
-                      </time>
-                    </div>
-                  </li>
-                </ul>
-                <form className="reviews__form form" action="#" method="post">
-                  <label
-                    className="reviews__label form__label"
-                    htmlFor="review"
-                  >
-                    Your review
-                  </label>
-                  <div className="reviews__rating-form form__rating">
-                    <input
-                      className="form__rating-input visually-hidden"
-                      name="rating"
-                      value="5"
-                      id="5-stars"
-                      type="radio"
-                    />
-                    <label
-                      htmlFor="5-stars"
-                      className="reviews__rating-label form__rating-label"
-                      title="perfect"
-                    >
-                      <svg className="form__star-image" width="37" height="33">
-                        <use href="#icon-star" />
-                      </svg>
-                    </label>
-
-                    <input
-                      className="form__rating-input visually-hidden"
-                      name="rating"
-                      value="4"
-                      id="4-stars"
-                      type="radio"
-                    />
-                    <label
-                      htmlFor="4-stars"
-                      className="reviews__rating-label form__rating-label"
-                      title="good"
-                    >
-                      <svg className="form__star-image" width="37" height="33">
-                        <use href="#icon-star" />
-                      </svg>
-                    </label>
-                    <input
-                      className="form__rating-input visually-hidden"
-                      name="rating"
-                      value="3"
-                      id="3-stars"
-                      type="radio"
-                    />
-                    <label
-                      htmlFor="3-stars"
-                      className="reviews__rating-label form__rating-label"
-                      title="not bad"
-                    >
-                      <svg className="form__star-image" width="37" height="33">
-                        <use href="#icon-star"></use>
-                      </svg>
-                    </label>
-
-                    <input
-                      className="form__rating-input visually-hidden"
-                      name="rating"
-                      value="2"
-                      id="2-stars"
-                      type="radio"
-                    />
-                    <label
-                      htmlFor="2-stars"
-                      className="reviews__rating-label form__rating-label"
-                      title="badly"
-                    >
-                      <svg className="form__star-image" width="37" height="33">
-                        <use href="#icon-star" />
-                      </svg>
-                    </label>
-
-                    <input
-                      className="form__rating-input visually-hidden"
-                      name="rating"
-                      value="1"
-                      id="1-star"
-                      type="radio"
-                    />
-                    <label
-                      htmlFor="1-star"
-                      className="reviews__rating-label form__rating-label"
-                      title="terribly"
-                    >
-                      <svg className="form__star-image" width="37" height="33">
-                        <use href="#icon-star" />
-                      </svg>
-                    </label>
-                  </div>
-                  <textarea
-                    className="reviews__textarea form__textarea"
-                    id="review"
-                    name="review"
-                    placeholder="Tell how was your stay, what you like and what can be improved"
-                  ></textarea>
-                  <div className="reviews__button-wrapper">
-                    <p className="reviews__help">
-                      To submit review please make sure to set
-                      <span className="reviews__star">rating</span> and describe
-                      your stay with at least
-                      <b className="reviews__text-amount">50 characters</b>.
-                    </p>
-                    <button
-                      className="reviews__submit form__submit button"
-                      type="submit"
-                      disabled=""
-                    >
-                      Submit
-                    </button>
-                  </div>
-                </form>
+                <ReviewList reviews={reviews} />
+                <ReviewForm />
               </section>
             </div>
           </div>
@@ -327,6 +142,7 @@ const OfferScreen = ({offers}) => {
 
 OfferScreen.propTypes = {
   offers: PropTypes.arrayOf(offerType).isRequired,
+  reviews: PropTypes.arrayOf(reviewType).isRequired,
 };
 
 export default OfferScreen;
