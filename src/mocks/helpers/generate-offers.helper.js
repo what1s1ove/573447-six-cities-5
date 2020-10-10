75f071a0eceded99d29f49bb9f1f239d09841b22
what1s1ove <zubko.2007@gmail.com>
2020-10-10 13:11:02 +0300

    module3-task1: + generate offer descriptions

--- src/mocks/helpers/generate-offers.helper.js
+++ src/mocks/helpers/generate-offers.helper.js
@@ -1,4 +1,9 @@
-import {getRandomItem, getRandomNumber, generateRandomId} from '~/helpers/helpers';
+import {
+  getRandomItem,
+  getRandomItems,
+  getRandomNumber,
+  generateRandomId,
+} from '~/helpers/helpers';
 import {OfferCity, OfferType} from '~/common/enums/enums';
 
 const OfferConfig = {
@@ -8,6 +13,12 @@ const OfferConfig = {
     `Canal View Prinsengracht`,
     `Nice, cozy, warm big bed apartment`,
   ],
+  DESCRIPTIONS: [
+    `Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
+    `It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.`,
+    `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    `If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.`,
+  ],
   PRICE: {
     MIN: 50,
     MAX: 250,
@@ -16,6 +27,17 @@ const OfferConfig = {
     MIN: 0,
     MAX: 5,
     FRACTIONAL_PART_COUNT: 1,
+  },
+  IMG: {
+    IMG_MIN_COUNT: 3,
+    IMG_MAX_COUNT: 6,
+    IMG_PATHS: [
+      `img/apartment-01.jpg`,
+      `img/apartment-02.jpg`,
+      `img/apartment-03.jpg`,
+      `img/studio-01.jpg`,
+      `img/room.jpg`,
+    ],
   },
 };
 
@@ -27,11 +49,20 @@ const generateOffer = () => ({
   city: getRandomItem(cities),
   type: getRandomItem(offerTypes),
   title: getRandomItem(OfferConfig.TITLES),
-  price: getRandomNumber(OfferConfig.PRICE.MIN, OfferConfig.PRICE.MAX),
   rating: getRandomNumber(
       OfferConfig.RATING.MIN,
       OfferConfig.RATING.MAX,
       OfferConfig.RATING.FRACTIONAL_PART_COUNT
+  ),
+  description: getRandomItem(OfferConfig.DESCRIPTIONS),
+  price: getRandomNumber(OfferConfig.PRICE.MIN, OfferConfig.PRICE.MAX),
+  imagePreview: getRandomItem(OfferConfig.IMG.IMG_PATHS),
+  images: getRandomItems(
+      OfferConfig.IMG.IMG_PATHS,
+      getRandomNumber(
+          OfferConfig.IMG.IMG_MIN_COUNT,
+          OfferConfig.IMG.IMG_MAX_COUNT
+      )
   ),
 });
 
