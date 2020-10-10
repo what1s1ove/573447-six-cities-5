35e3de072eb079b37f58a432257d14a4f007e97a
what1s1ove <zubko.2007@gmail.com>
2020-10-10 17:40:01 +0300

    module3-task1: + review-item component

---
+++ src/components/review-list/review-list.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import ReviewItem from '~/components/review-item/review-item';
+
+const ReviewList = () => (
+  <ul className="reviews__list">
+    <ReviewItem />
+  </ul>
+);
+
+export default ReviewList;
