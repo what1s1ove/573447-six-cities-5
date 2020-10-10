252685a3d49e5aaeaeb78422656bd21ba72a7447
what1s1ove <zubko.2007@gmail.com>
2020-10-10 18:51:10 +0300

    module3-task1: * reviews-list component

--- src/components/review-list/review-list.jsx
+++ src/components/review-list/review-list.jsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
+import {reviewType} from '~/common/prop-types/prop-types';
 import ReviewItem from '~/components/review-item/review-item';
 
-const ReviewList = () => (
+const ReviewList = ({reviews}) => (
   <ul className="reviews__list">
-    <ReviewItem />
+    {reviews.map((review) => (
+      <ReviewItem review={review} key={review.id} />
+    ))}
   </ul>
 );
+
+ReviewList.propTypes = {
+  reviews: PropTypes.arrayOf(reviewType).isRequired,
+};
 
 export default ReviewList;
