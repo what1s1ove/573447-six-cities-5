0d9996f6dfe6c006c369a7a616972f77676fb2a9
what1s1ove <zubko.2007@gmail.com>
2020-10-10 18:57:56 +0300

    module3-task1: * reviews-item component

--- src/components/review-item/review-item.jsx
+++ src/components/review-item/review-item.jsx
@@ -1,39 +1,43 @@
 import * as React from 'react';
+import {reviewType} from '~/common/prop-types/prop-types';
+import {getRatingInPercents} from '~/helpers/offer';
+import {getFormattedDate} from './helpers';
 
-const ReviewItem = () => (
+const ReviewItem = ({review}) => (
   <li className="reviews__item">
     <div className="reviews__user user">
       <div className="reviews__avatar-wrapper user__avatar-wrapper">
         <img
           className="reviews__avatar user__avatar"
-          src="img/avatar-max.jpg"
+          src={review.user.avatarUrl}
           width="54"
           height="54"
           alt="Reviews avatar"
         />
       </div>
-      <span className="reviews__user-name"> Max </span>
+      <span className="reviews__user-name">{review.user.name}</span>
     </div>
     <div className="reviews__info">
       <div className="reviews__rating rating">
         <div className="reviews__stars rating__stars">
           <span
             style={{
-              width: `80%`,
+              width: `${getRatingInPercents(review.rating)}%`,
             }}
           ></span>
           <span className="visually-hidden">Rating</span>
         </div>
       </div>
-      <p className="reviews__text">
-        A quiet cozy and picturesque that hides behind a a river by the unique
-        lightness of Amsterdam. The building is green and from 18th century.
-      </p>
-      <time className="reviews__time" dateTime="2019-04-24">
-        April 2019
+      <p className="reviews__text">{review.comment}</p>
+      <time className="reviews__time" dateTime={review.date.toISOString()}>
+        {getFormattedDate(review.date)}
       </time>
     </div>
   </li>
 );
+
+ReviewItem.propTypes = {
+  review: reviewType,
+};
 
 export default ReviewItem;
