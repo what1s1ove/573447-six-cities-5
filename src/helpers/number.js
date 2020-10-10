5bf3925e5e7634f0feb7f2631f5602651325a4c6
what1s1ove <zubko.2007@gmail.com>
2020-10-10 13:09:57 +0300

    module3-task1: * get-random numbers helper

--- src/helpers/number.js
+++ src/helpers/number.js
@@ -1,9 +1,9 @@
 const getRandomNumber = (min, max, fractionalPartCount = 0) => {
-  const randomNumber = Number(
-      (Math.random() * (max - min + 1) + min).toFixed(fractionalPartCount)
-  );
+  const randomNumber = min + Math.random() * (max + 1 - min);
 
-  return randomNumber;
+  return fractionalPartCount
+    ? Number(randomNumber.toFixed(fractionalPartCount))
+    : Math.floor(randomNumber);
 };
 
 export {getRandomNumber};
