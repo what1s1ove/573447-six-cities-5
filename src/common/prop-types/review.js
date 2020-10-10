bba7c8f2e5e7fb1bdff940486e14fbff65bdb688
what1s1ove <zubko.2007@gmail.com>
2020-10-10 18:50:38 +0300

    module3-task1: * review type

--- src/common/prop-types/review.js
+++ src/common/prop-types/review.js
@@ -1,12 +1,12 @@
 import PropType from 'prop-types';
 import {userType} from '~/common/prop-types/user';
 
-const commentType = PropType.exact({
+const reviewType = PropType.exact({
   user: userType,
-  id: PropType.id.isRequired,
+  id: PropType.string.isRequired,
   rating: PropType.number.isRequired,
   comment: PropType.string.isRequired,
-  date: PropType.date.isRequired,
+  date: PropType.object.isRequired,
 }).isRequired;
 
-export {commentType};
+export {reviewType};
