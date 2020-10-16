5bf07ed92c5fd0ca1471065902f6ae2e3132da45
what1s1ove <zubko.2007@gmail.com>
2020-10-16 08:00:29 +0300

    module4-task1: * generate city location

--- src/mocks/helpers/generate-offer-location.helper.js
+++ src/mocks/helpers/generate-offer-location.helper.js
@@ -1,33 +1,42 @@
-import {getRandomNumber, getRandomItem} from '~/helpers/helpers';
+import {getRandomNumber} from '~/helpers/helpers';
 
 const OfferLocationConfig = {
-  COORDS: {
-    LATITUDES: [
-      52.3909553943508,
-      52.369553943508,
-      52.3909553943508,
-      52.3809553943508,
-    ],
-    LONGITUDES: [
-      4.85309666406198,
-      4.85309666406198,
-      4.929309666406198,
-      4.939309666406198,
-    ],
-  },
+  COORDS: [
+    {
+      latitude: 52.3909553943508,
+      longitude: 4.85309666406198,
+    },
+    {
+      latitude: 52.369553943508,
+      longitude: 4.85309666406198,
+    },
+    {
+      latitude: 52.3909553943508,
+      longitude: 4.929309666406198,
+    },
+    {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+    },
+  ],
+
   ZOOM: {
     MIN: 5,
     MAX: 10,
   },
 };
 
-const generateOfferLocation = () => ({
-  latitude: getRandomItem(OfferLocationConfig.COORDS.LATITUDES),
-  longitude: getRandomItem(OfferLocationConfig.COORDS.LONGITUDES),
-  zoom: getRandomNumber(
-      OfferLocationConfig.ZOOM.MIN,
-      OfferLocationConfig.ZOOM.MAX
-  ),
-});
+const generateOfferLocation = (offerIdx) => {
+  const {latitude, longitude} = OfferLocationConfig.COORDS[offerIdx];
+
+  return {
+    latitude,
+    longitude,
+    zoom: getRandomNumber(
+        OfferLocationConfig.ZOOM.MIN,
+        OfferLocationConfig.ZOOM.MAX
+    ),
+  };
+};
 
 export {generateOfferLocation};
