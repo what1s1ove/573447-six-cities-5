a44fbaabed151e95d7cc860cb3174751165264de
what1s1ove <zubko.2007@gmail.com>
2020-10-21 16:36:40 +0300

    module5-task1: + more cities options

--- src/mocks/helpers/generate-offer-city.helper.js
+++ src/mocks/helpers/generate-offer-city.helper.js
@@ -6,6 +6,26 @@ const cityToLocationMap = {
     longitude: 4.9,
     zoom: 9,
   },
+  [OfferCity.PARIS]: {
+    latitude: 48.85661,
+    longitude: 2.351499,
+    zoom: 13,
+  },
+  [OfferCity.BRUSSELS]: {
+    latitude: 50.846557,
+    longitude: 4.351697,
+    zoom: 13,
+  },
+  [OfferCity.HAMBURG]: {
+    latitude: 53.550341,
+    longitude: 10.000654,
+    zoom: 13,
+  },
+  [OfferCity.COLOGNE]: {
+    latitude: 50.938361,
+    longitude: 6.959974,
+    zoom: 13,
+  },
 };
 
 const generateOfferCity = (city) => ({
