7d2a448f79435ddcdef3b66185cd1803f2066e6c
what1s1ove <zubko.2007@gmail.com>
2020-10-31 13:04:07 +0200

    module7-task1: - locations from places reducer

--- src/store/reducers/places-data/places-data.js
+++ src/store/reducers/places-data/places-data.js
@@ -1,13 +1,10 @@
-import {getUniqueOfferCities} from '~/helpers/helpers';
 import {generateOffers} from '~/mocks/helpers/helpers';
 import {AppConfig} from '~/common/enums/enums';
 
 const offers = generateOffers(AppConfig.OFFERS_COUNT);
-const locations = getUniqueOfferCities(offers);
 
 const initialState = {
   offers,
-  locations,
 };
 
 const placesDataReducer = (state = initialState) => {
