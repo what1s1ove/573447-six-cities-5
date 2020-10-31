89dff2609d44661ae857c4edaa1d60ef78c0e82b
what1s1ove <zubko.2007@gmail.com>
2020-10-31 15:14:49 +0200

    module7-task2: + user reducer

--- src/store/reducer.root.js
+++ src/store/reducer.root.js
@@ -1,8 +1,9 @@
 import {combineReducers} from 'redux';
-import {placesDataReducer} from '~/store/reducers/reducers';
+import {placesDataReducer, userReducer} from '~/store/reducers/reducers';
 
 const rootReducer = combineReducers({
   data: placesDataReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
