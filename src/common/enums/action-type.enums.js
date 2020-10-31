12bba5657608af8b5c96758ee6600bd890504284
what1s1ove <zubko.2007@gmail.com>
2020-10-31 17:32:42 +0200

    module7-task3: + placeaction type

--- src/common/enums/action-type.enums.js
+++ src/common/enums/action-type.enums.js
@@ -2,10 +2,16 @@ const PlacesDataActionType = {
   LOAD_OFFERS: `LOAD_OFFERS`,
 };
 
+const PlaceActionType = {
+  LOAD_OFFER: `LOAD_OFFER`,
+  LOAD_COMMENTS: `LOAD_COMMENTS`,
+  UPLOAD_COMMENT: `UPLOAD_COMMENT`,
+};
+
 const UserActionType = {
   SET_AUTH_STATUS: `SET_AUTH_STATUS`,
   SET_USER: `SET_USER`,
   REDIRECT_TO_ROUTE: `REDIRECT_TO_ROUTE`,
 };
 
-export {PlacesDataActionType, UserActionType};
+export {PlacesDataActionType, PlaceActionType, UserActionType};
