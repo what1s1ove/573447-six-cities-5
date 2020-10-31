0e6113c4824162bcaac792d2e83884419433bc6b
what1s1ove <zubko.2007@gmail.com>
2020-10-31 17:40:38 +0200

    module7-task3: + place selectors

---
+++ src/store/selectors/place/place.js
@@ -0,0 +1,7 @@
+const getOffer = ({place}) => place.offer;
+
+const getComments = ({place}) => place.comments;
+
+const getSimilarOffers = ({place}) => place.similarOffers;
+
+export {getOffer, getComments, getSimilarOffers};
