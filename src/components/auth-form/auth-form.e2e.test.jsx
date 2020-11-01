05129be76726d8c24770904a7f288b1db4924ef1
what1s1ove <zubko.2007@gmail.com>
2020-11-01 17:30:04 +0200

    module8-task1: + e2e test for auth-form component

---
+++ src/components/auth-form/auth-form.e2e.test.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import {shallow} from 'enzyme';
+import AuthForm from './auth-form';
+
+describe(`AuthForm component`, () => {
+  it(`should call callback when from submit`, () => {
+    const handleFormSubmit = jest.fn();
+    const component = shallow(
+        <AuthForm
+          formState={{}}
+          onFormSubmit={handleFormSubmit}
+          onFormStateChange={jest.fn()}
+        />
+    );
+
+    component.find(`.login__form`).simulate(`submit`, {
+      preventDefault: jest.fn(),
+    });
+
+    expect(handleFormSubmit).toHaveBeenCalled();
+  });
+});
