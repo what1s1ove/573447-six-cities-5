a8c5153cafe4568101a164acedfc4b5f4d36a036
what1s1ove <zubko.2007@gmail.com>
2020-11-01 15:55:42 +0200

    module8-task1: + mocked city

---
+++ src/mocks/city/city.js
@@ -0,0 +1,10 @@
+const mockedCity = {
+  name: `Dusseldorf`,
+  location: {
+    latitude: 51.225402,
+    longitude: 6.776314,
+    zoom: 13,
+  },
+};
+
+export {mockedCity};
--- src/mocks/mocks.js
+++ src/mocks/mocks.js
@@ -1,3 +1,5 @@
 export * from './offers/offers';
 export * from './user/user';
+export * from './reviews/reviews';
+export * from './city/city';
 export * from './store/store';
