20fd1bb6b343c3e3006a87afaa01900536c04bf3
what1s1ove <zubko.2007@gmail.com>
2020-11-01 18:06:55 +0200

    module8-task1: + e2e test for review-rating component

---
+++ src/components/review-rating/review-rating.e2e.test.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import {shallow} from 'enzyme';
+import ReviewRating from './review-rating';
+
+describe(`ReviewRating component`, () => {
+  it(`should call callback when rating was changed`, () => {
+    const handleRatingChange = jest.fn();
+    const component = shallow(
+        <ReviewRating
+          value="value"
+          name="name"
+          isChecked={false}
+          onRatingChange={handleRatingChange}
+        />
+    );
+
+    component.find(`.form__rating-input`).simulate(`change`, {
+      target: {
+        value: `value`,
+      },
+    });
+
+    expect(handleRatingChange).toHaveBeenCalled();
+  });
+});
