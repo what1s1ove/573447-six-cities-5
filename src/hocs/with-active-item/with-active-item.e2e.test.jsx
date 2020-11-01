4b4c45dad3aafad4e3146c3f35d4bab09602b27d
what1s1ove <zubko.2007@gmail.com>
2020-11-01 18:31:07 +0200

    module8-task1: + e2e test for withFormEditing hoc

--- src/hocs/with-active-item/with-active-item.e2e.test.jsx
+++ src/hocs/with-active-item/with-active-item.e2e.test.jsx
@@ -8,19 +8,16 @@ const WrappedMockedComponent = withActiveItem(MockedComponent);
 
 describe(`withActiveItem hoc`, () => {
   it(`should correctly set active item`, () => {
-    const handleActiveItemChange = jest.fn();
     const wrapper = shallow(
         <WrappedMockedComponent
           activeItem={null}
-          onActiveItemChange={handleActiveItemChange}
+          onActiveItemChange={jest.fn()}
         />
     );
 
     expect(wrapper.props().activeItem).toBeNull();
 
     wrapper.props().onActiveItemChange(mockedOffer);
-
-    expect(handleActiveItemChange).toHaveBeenCalled();
 
     expect(wrapper.props().activeItem).toBe(mockedOffer);
   });
