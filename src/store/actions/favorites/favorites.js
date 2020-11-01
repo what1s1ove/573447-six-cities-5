65a26676a3df7e27efabe27b30ef26fb1d738545
what1s1ove <zubko.2007@gmail.com>
2020-11-01 10:08:32 +0200

    module7-task3: + favorites ac

---
+++ src/store/actions/favorites/favorites.js
@@ -0,0 +1,47 @@
+import {FavoritesActionType, OfferFavoriteStatus} from '~/common/enums/enums';
+import {adaptOffersToClient, adaptOfferToClient, extendObject} from '~/helpers/helpers';
+
+const FavoritesActionCreator = {
+  loadFavorites: (offers) => ({
+    type: FavoritesActionType.LOAD_FAVORITES,
+    payload: {
+      offers,
+    },
+  }),
+  updateFavorite: (offer) => ({
+    type: FavoritesActionType.UPDATE_FAVORITE,
+    payload: {
+      offer,
+    },
+  }),
+  fetchFavorites: () => (dispatch, _, {api}) => {
+    api
+      .get(`/favorite`)
+      .then(({data}) =>
+        dispatch(FavoritesActionCreator.loadFavorites(adaptOffersToClient(data)))
+      )
+      .catch((err) => {
+        throw err;
+      });
+  },
+  toggleFavorite: (offer) => (dispatch, _, {api}) => {
+    dispatch(FavoritesActionCreator.updateFavorite(extendObject(offer, {
+      isSaving: true
+    })));
+
+    api
+      .post(`/favorite/${offer.id}/${offer.isFavorite ? OfferFavoriteStatus.FALSE : OfferFavoriteStatus.TRUE}`)
+      .then(({data}) =>
+        dispatch(FavoritesActionCreator.updateFavorite(adaptOfferToClient(data)))
+      )
+      .catch((err) => {
+        dispatch(FavoritesActionCreator.updateFavorite(extendObject(offer, {
+          isSaving: false
+        })));
+
+        throw err;
+      });
+  }
+};
+
+export {FavoritesActionCreator};
