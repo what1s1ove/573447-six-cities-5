aaf1c957894b158baba9fd4bf8ac66d0df9715fb
what1s1ove <zubko.2007@gmail.com>
2020-11-02 20:06:14 +0200

    module8-task1: + mocked fetched user

--- src/mocks/user/user.js
+++ src/mocks/user/user.js
@@ -6,4 +6,13 @@ const mockedUser = {
   name: `Oliver`,
 };
 
-export {mockedUser};
+const mockedFetchedUser = {
+  'avatar_url': `https://assets.htmlacademy.ru/intensives/javascript-3/avatar/2.jpg`,
+  'email': `oliver@mail.com`,
+  'id': 1,
+  'is_pro': false,
+  'name': `Oliver`,
+};
+
+
+export {mockedUser, mockedFetchedUser};
