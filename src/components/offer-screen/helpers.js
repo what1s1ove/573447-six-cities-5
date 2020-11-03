f04e9516265c6b6747145462debf31137159084e
what1s1ove <zubko.2007@gmail.com>
2020-11-03 19:27:45 +0200

    module7-task3: + sort reviews functionality

--- src/components/offer-screen/helpers.js
+++ src/components/offer-screen/helpers.js
@@ -1,7 +1,10 @@
+import {getSortedItems} from '~/helpers/helpers';
+
 const MAX_REVIEWS_COUNT = 10;
 
 const getFilteredReviews = (reviews) => {
-  const filteredReviews = reviews.slice(0, MAX_REVIEWS_COUNT);
+  const sortedReviews = getSortedItems(reviews, (reviewA, reviewB) => reviewB.date - reviewA.date);
+  const filteredReviews = sortedReviews.slice(0, MAX_REVIEWS_COUNT);
 
   return filteredReviews;
 };
