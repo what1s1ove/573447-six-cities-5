5dd61910991c8b48281ea94a5c0c4293cf35fb29
what1s1ove <zubko.2007@gmail.com>
2020-11-04 08:42:12 +0200

    module8-task1: * snapshot test for private-route component

--- src/components/private-route/private-route.test.jsx
+++ src/components/private-route/private-route.test.jsx
@@ -3,14 +3,17 @@ import {render} from 'enzyme';
 import {Provider} from 'react-redux';
 import {MemoryRouter} from 'react-router-dom';
 import {store} from '~/mocks/mocks';
+import {AppRoute} from '~/common/enums/enums';
 import PrivateRoute from './private-route';
+
+const PrivateComponent = () => <p>Private Component</p>;
 
 describe(`PrivateRoute component`, () => {
   it(`should render PrivateRoute component`, () => {
     const component = render(
         <Provider store={store}>
           <MemoryRouter>
-            <PrivateRoute />
+            <PrivateRoute path={AppRoute.MAIN} component={PrivateComponent} />
           </MemoryRouter>
         </Provider>
     );
