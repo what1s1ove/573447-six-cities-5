ecf1b3eebd93f48edaa9aa9a8524f9cd43dbff30
what1s1ove <zubko.2007@gmail.com>
2020-11-04 19:06:56 +0200

    module8-task1: + test for place ac

--- src/store/actions/place/place.test.js
+++ src/store/actions/place/place.test.js
@@ -1,6 +1,5 @@
 import MockAdapter from 'axios-mock-adapter';
 import {createAPI} from '~/services/api/api';
-import {adaptOffersToClient, adaptOfferToClient, adaptReviewsToClient} from '~/helpers/helpers';
 import {
   mockedOffers,
   mockedOffer,
@@ -9,7 +8,12 @@ import {
   mockedReviews,
   mockedFetchedReviews,
 } from '~/mocks/mocks';
-import {FavoritesActionType, OfferFavoriteStatus, PlaceActionType} from '~/common/enums/enums';
+import {
+  adaptOffersToClient,
+  adaptOfferToClient,
+  adaptReviewsToClient,
+} from '~/helpers/helpers';
+import {OfferFavoriteStatus, PlaceActionType} from '~/common/enums/enums';
 import {PlaceActionCreator} from './place';
 
 const api = createAPI({
@@ -86,6 +90,83 @@ describe(`Place action creator works correctly`, () => {
         type: PlaceActionType.LOAD_REVIEWS,
         payload: {
           reviews: adaptReviewsToClient(mockedFetchedReviews),
+        },
+      });
+    });
+  });
+
+  it(`fetchSimilarOffers ac returns correct action. Should make a correct API call to /hotels`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const fetchSimilarOffersLoader = PlaceActionCreator.fetchSimilarOffers(mockedFetchedOffer.id);
+
+    apiMock.onGet(`/hotels/${mockedFetchedOffer.id}/nearby`).reply(200, mockedFetchedOffers);
+
+    return fetchSimilarOffersLoader(dispatch, jest.fn(), {api}).then(() => {
+      expect(dispatch).toHaveBeenCalledTimes(1);
+
+      expect(dispatch).toHaveBeenNthCalledWith(1, {
+        type: PlaceActionType.LOAD_SIMILAR_OFFERS,
+        payload: {
+          similarOffers: adaptOffersToClient(mockedFetchedOffers),
+        },
+      });
+    });
+  });
+
+  it(`uploadReview ac returns correct action. Should make a correct API call to /comments`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const fakeComment = {comment: `Comment`, rating: `5`};
+    const uploadReviewLoader = PlaceActionCreator.uploadReview(mockedOffer.id, fakeComment);
+
+    apiMock.onPost(`/comments/${mockedOffer.id}`).reply(200, mockedFetchedReviews);
+
+    return uploadReviewLoader(dispatch, jest.fn(), {api}).then(() => {
+      expect(dispatch).toHaveBeenCalledTimes(1);
+
+      expect(dispatch).toHaveBeenNthCalledWith(1, {
+        type: PlaceActionType.LOAD_REVIEWS,
+        payload: {
+          reviews: adaptReviewsToClient(mockedFetchedReviews),
+        },
+      });
+    });
+  });
+
+  it(`toggleFavorite ac returns correct action. Should make a correct API call to /favorite`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const toggleFavoriteLoader = PlaceActionCreator.toggleFavorite(mockedOffer);
+
+    apiMock.onPost(`/favorite/${mockedOffer.id}/${OfferFavoriteStatus.TRUE}`).reply(200, mockedFetchedOffer);
+
+    return toggleFavoriteLoader(dispatch, jest.fn(), {api}).then(() => {
+      expect(dispatch).toHaveBeenCalledTimes(1);
+
+      expect(dispatch).toHaveBeenNthCalledWith(1, {
+        type: PlaceActionType.LOAD_OFFER,
+        payload: {
+          offer: adaptOfferToClient(mockedFetchedOffer),
+        },
+      });
+    });
+  });
+
+  it(`toggleSimilarOfferFavorite ac returns correct action. Should make a correct API call to /favorite`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const toggleSimilarOfferFavoriteLoader = PlaceActionCreator.toggleSimilarOfferFavorite(mockedOffer);
+
+    apiMock.onPost(`/favorite/${mockedOffer.id}/${OfferFavoriteStatus.TRUE}`).reply(200, mockedFetchedOffer);
+
+    return toggleSimilarOfferFavoriteLoader(dispatch, jest.fn(), {api}).then(() => {
+      expect(dispatch).toHaveBeenCalledTimes(1);
+
+      expect(dispatch).toHaveBeenNthCalledWith(1, {
+        type: PlaceActionType.UPDATE_SIMILAR_OFFER,
+        payload: {
+          offer: adaptOfferToClient(mockedFetchedOffer),
         },
       });
     });
